4d816d34a42ad31689b9b270b12f6c28eb2eaa65
pwb8 <74422199+pwb8@users.noreply.github.com>
2021-07-29 09:56:13 -0400

    Update irt simulation(1).R

--- irt simulation(1).R
+++ irt simulation(1).R
@@ -16,7 +16,7 @@ library(catIrt)
 #cbind = column binding; takes vectors and binds it to a matrix of 3 arguments 
 #rep = repeat 
 itpar1 <- cbind(a = rep(1,100), b = rep(0,100), c = 0)
-
+itpar4 <-
 # create a second matrix of item parameters named itpar2,
 # for which half of the items have difficulty = 0 and 
 # the remaining half are easier by 2 logits
@@ -41,6 +41,7 @@ itresp3 <- matrix(data = NA, nrow = 200, ncol = 100, byrow = T)
 simIrt(theta = -1, params = itpar1, mod = "brm")
 for (i in 1:200) {
 itresp1[i,] <- simIrt(theta = -1, params = itpar1, mod = "brm")$resp
+mean(itresp1)
 itresp2[i,] <- simIrt(theta = 0, params = itpar1, mod = "brm")$resp
 itresp3[i,] <- simIrt(theta = -1, params = itpar2, mod = "brm")$resp
 } #review and manipulate it! Also look at lines 45-59
@@ -51,20 +52,33 @@ eap2 <- matrix(data = NA, nrow = 200, ncol = 1, byrow = T)
 eap3 <- matrix(data = NA, nrow = 200, ncol = 1, byrow = T)
 
 # compute the score estimates and fill the matrices with them
+?eapEst #estimated ability; Bayesian-Model Estimation; Expected-A-Posterior Estimation 
+##"brm" = fit bayesian generalrized non-linear model 
 eap1 <- eapEst(resp = itresp1, params = itpar1, range = c(-4, 4),
-               mod = "brm", ddist = dnorm, mean = 0, sd = 1, quad = 33)$theta
+               mod = "brm", ddist = dnorm, mean = 0, sd = 100, quad = 33)$theta
+##Notes
+#renamed dnorm as ddist which is normal distribution 
+#What does quad mean? 
+#standard deviation is 1 
+#by computing the score estimates on the first posteriori, we wouldn't need to 
+#add anything to eap 2 and eap3? 
 eap2 <- eapEst(resp = itresp2, params = itpar1)$theta
 eap3 <- eapEst(resp = itresp3, params = itpar1)$theta
 
-# find the mean of each condition
-mean(eap1)
-mean(eap2)
-mean(eap3)
 
-t.test(x = eap2,y = eap3, alternative = "two.sided")
+# find the mean of each condition
+mean(eap1) #-0.96081
+##general ability score = -1 <--referring to theta =-1
+mean(eap2) #0.004706
+###posterior is pulled into the mean
+###shrinkage --> shrinks towards the mean of the prior
+mean(eap3) #0.0033115 (mean is the smallest)
+
+t.test(x = eap2,y = eap3, alternative = "two.sided") #Welch Two Sample t-test
 
 
 # nothing well-developed below this line, just stating to think about how we might simulate CAT administration
+
 catgen <- list()
 catspec <- list()
 
@@ -117,5 +131,9 @@ catspec[i] <- catIrt( itpar1, mod = c("brm"),
                                   n.min = 30, n.max = 50 ),
                   ddist = dnorm,
                   progress = TRUE )
-
 }
+# independent variables : time point, type of treatment, type of assessment 
+# time point: pre-treatment and post-treatment 
+# type of treatment: item-specific,  item-general, "in between"
+# type of assessment: CAT, static, PNT-CAT, PNT-static 
+# dependent variables: treatment effect size, naming performance 
